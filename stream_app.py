113b7d34aa39da47ae4b5be3e34d85c87a2a4ffe
mintygral <mailmedhashinde@gmail.com>
2024-04-21 01:53:57 +0000

    Fix video upload and change title

--- stream_app.py
+++ stream_app.py
@@ -1,26 +1,25 @@
 import streamlit as st
 import base64
-import numpy as np 
+
 def videoPlayback(video, loop, autostart):
     video_bytes = video.read()
-
     video_base64 = base64.b64encode(video_bytes).decode('utf-8')
 
-    if loop == True and autostart == True:
+    if loop and autostart:
         video_html = f"""
             <video loop autoplay controls style="max-width: 100%; height: auto;">
             <source src="data:video/{video.type};base64,{video_base64}" type="{video.type}">
             Your browser does not support the video tag.
             </video>
             """
-    elif loop == True and autostart == False:
+    elif loop:
         video_html = f"""
         <video loop controls style="max-width: 100%; height: auto;">
             <source src="data:video/{video.type};base64,{video_base64}" type="{video.type}">
             Your browser does not support the video tag.
         </video>
         """
-    elif autostart == True and loop == False:
+    elif autostart:
         video_html = f"""
         <video autoplay controls style="max-width: 100%; height: auto;"> 
             <source src="data:video/{video.type};base64,{video_base64}" type="{video.type}">
@@ -29,31 +28,28 @@ def videoPlayback(video, loop, autostart):
         """
     else: 
         return st.video(video)
-    return st.markdown(video_html, unsafe_allow_html = True)
-def videoInGrid(num_columns, videos, autostart, loop):
-    num_columns = num_columns
-    num_objects_per_column = int(np.ceil(len(videos) / num_columns))
-    col = st.columns(num_columns)
     
-    for i in range(num_columns):
-        with col[i]:
-            for j in range(i * num_objects_per_column, (i + 1) * num_objects_per_column):
-                videoPlayback(video=videos[j], autostart=autostart, loop=loop)
+    st.markdown(video_html, unsafe_allow_html=True)
+
 def main():
     st.set_page_config(
-        page_title="My Streamlit App",
+        page_title="Automatic Construction Site Monitoring: Advanced Video Analytics for Activity Detection",
         layout="wide",
         initial_sidebar_state="expanded"
     )
-    st.title("Video Showing Test App")
-    video = st.file_uploader('Upload the video', type =['.mp4'], accept_multiple_files=True)
-    if video is not None:
-        videoInGrid(
-            num_columns = 2,
-            videos = video,
-            autostart=True,
-            loop=True
-        )
+    st.title("Automatic Construction Site Monitoring: Advanced Video Analytics for Activity Detection")
+    
+    video = st.file_uploader('Upload the video', type=['.mp4'], accept_multiple_files=True)
+    
+    if video:
+        st.write("Video uploaded successfully!")
+        if len(video) > 0:  # Check if there's at least one video uploaded
+            st.write("Uploaded Video:")
+            videoPlayback(video[0], loop=True, autostart=True)
+            st.button("Analyze Video")
+    else:
+        st.write("Upload a video")
+
     
 if __name__ =="__main__":
     main()
